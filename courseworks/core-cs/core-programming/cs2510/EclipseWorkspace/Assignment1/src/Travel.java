b3bcd66c2ede11db3b4d2671bc2d2f8513a72595
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-05-20 07:05:45 +0600

    Assignemnt 1 completed

--- courseworks/core-cs/core-programming/cs2510/EclipseWorkspace/Assignment1/src/Travel.java
+++ courseworks/core-cs/core-programming/cs2510/EclipseWorkspace/Assignment1/src/Travel.java
@@ -18,6 +18,50 @@
                                 +----------------+     +----------------------+
    */
 
+interface Housing { }
+
+class Hut implements Housing {
+  int capacity;
+  int population;
+  
+  // constructor
+  Hut(int capacity, int population) {
+    this.capacity = capacity;
+    this.population = population;
+  }
+}
+
+class Inn implements Housing {
+  String name;
+  int capacity;
+  int population;
+  int stable;
+  
+  // constructor
+  Inn(String name, int capacity, int population, int stable) {
+    this.name = name;
+    this.capacity = capacity;
+    this.population = population;
+    this.stable = stable;
+  }
+}
+
+// represents a castle
+class Castle implements Housing {
+  String name;
+  String familyName;
+  int population;
+  int carriageCapacity;
+  
+  // constructor
+  Castle(String name, String familyName, int population, int carriageCapacity) {
+    this.name = name;
+    this.familyName = familyName;
+    this.population = population;
+    this.carriageCapacity = carriageCapacity;
+  }
+}
+
 /*
                     +----------------+
                     | Transportation |
@@ -37,3 +81,56 @@
       | Housing to    |         +---------------+
       +---------------+
  */
+
+// represents a transportation
+interface Transportation { }
+
+// represents a horse
+class Horse implements Transportation {
+  String name;
+  String color;
+  Housing from;
+  Housing to;
+  
+  // constructor
+  Horse(String name, String color, Housing from, Housing to) {
+    this.name = name;
+    this.color = color;
+    this.from = from;
+    this.to = to;
+  }
+}
+
+// represents a Carriage
+class Carriage implements Transportation {
+  int tonnage;
+  Housing from;
+  Housing to;
+  
+  // constructor
+  Carriage(int tonnage, Housing from, Housing to) {
+    this.tonnage = tonnage;
+    this.from = from;
+    this.to = to;
+  }
+}
+
+
+// examples and tests
+class ExamplesTravel {
+  // examples of Housing
+  Housing hut1 = new Hut(5, 1);
+  Housing hut2 = new Hut(8, 2);
+
+  Housing inn1 = new Inn("Inn At The Crossroads", 40, 20, 12);
+  Housing inn2 = new Inn("Inn At The Wedding", 100, 80, 10);
+  
+  Housing castle1 = new Castle("Winterfell", "Stark", 500, 6);
+  Housing castle2 = new Castle("Eilean Donan", "MacRae", 90, 9);
+  
+  Transportation horse1 = new Horse("Pony", "Palomino", inn1, castle1);
+  Transportation horse2 = new Horse("AQHA Gelding", "Palomino", hut1, castle2);
+  
+  Transportation carriage1 = new Carriage(8, castle1, inn1);
+  Transportation carriage2 = new Carriage(12, castle2, inn2);
+}
